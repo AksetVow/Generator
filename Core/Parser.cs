6657971311441e9c03ae19960d5c4dbb56867184
rslyvka@gmail.com <rslyvka@gmail.com>
2015-10-22 01:04:44 +0300

    Add entities model

---
+++ Core/Parser.cs
@@ -0,0 +1,22 @@
+﻿using Core.Export;
+using Core.Import;
+using System;
+using System.Collections.Generic;
+
+namespace Core
+{
+    static class Parser
+    {
+        public static IEnumerable<Template> ParseExportSettings(string exportSetting)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static IEnumerable<ImportConfiguration> ParseImportSettings(string importSetting)
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
---
+++ Core/Settings.cs
@@ -0,0 +1,41 @@
+﻿using Core.Export;
+using Core.Import;
+using System.Collections.Generic;
+
+namespace Core
+{
+    static class Settings
+    {
+        private static List<Template> _templates = new List<Template>();
+        private static List<ImportConfiguration> _importConfigurations = new List<ImportConfiguration>();
+
+        public const string ExportIni = "";
+        public const string ImportIni = "";
+
+        static Settings()
+        {
+            var importConfigurations = Parser.ParseImportSettings(ImportIni);
+            _importConfigurations.AddRange(importConfigurations);
+
+            var templates = Parser.ParseExportSettings(ExportIni);
+            _templates.AddRange(templates);
+        }
+
+        public static IEnumerable<Template> Templates
+        { 
+            get 
+            {
+                return _templates;
+            }        
+        }
+
+        public static IEnumerable<ImportConfiguration> ImportConfigurations
+        {
+            get 
+            {
+                return _importConfigurations;
+            }
+        }
+
+    }
+}
