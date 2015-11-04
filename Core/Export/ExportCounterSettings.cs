71c98b15f47df744f19385041bf18d65d6f48710
rslyvka@gmail.com <rslyvka@gmail.com>
2015-11-05 01:26:48 +0200

    Modify Export Counter Settings

--- Core/Export/ExportCounterSettings.cs
+++ Core/Export/ExportCounterSettings.cs
@@ -3,5 +3,11 @@ namespace Core.Export
 {
     public class ExportCounterSettings
     {
+        public bool Author { get; set; }
+        public bool SourceCategory { get; set; }
+        public bool ThemaCategory { get; set; }
+        public bool PublicationDate { get; set; }
+        public bool Source { get; set; }
+        public bool Header { get; set; }
     }
 }
