3de6654f94bec7f887c31c84b763c2c7da550e79
Roman_Slyvka <roman_slyvka@epam.com>
2015-12-14 10:57:54 +0100

    Add tests for add category command

---
+++ TestCore/Tests/Commands/AddCategoryCommandTest.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Core;
+using System.Collections.Generic;
+using Core.Command.Commands;
+
+namespace TestCore.Tests.Commands
+{
+    [TestClass]
+    public class AddCategoryCommandTest
+    {
+        private AddCategoryCommand CreateStubAddCategoryCommand(out IList<Article> articles)
+        {
+            articles = new List<Article>();
+            articles.Add(new Article() { SubjectCategory = "Львів" });
+            articles.Add(new Article() { SubjectCategory = "Банки" });
+            articles.Add(new Article() { SubjectCategory = "Новини конкурентів" });
+
+            return new AddCategoryCommand("Київ", articles);
+        }
+
+
+        [TestMethod]
+        public void DoTest()
+        {
+            IList<Article> articles;
+
+            var addCategoryCommand = CreateStubAddCategoryCommand(out articles);
+            addCategoryCommand.Do();
+
+            Assert.AreEqual("Київ", articles[0].SubjectCategory);
+            Assert.AreEqual("Київ", articles[1].SubjectCategory);
+            Assert.AreEqual("Київ", articles[2].SubjectCategory);
+        }
+
+        [TestMethod]
+        public void UndoTest()
+        {
+            IList<Article> articles;
+
+            var addCategoryCommand = CreateStubAddCategoryCommand(out articles);
+            addCategoryCommand.Do();
+
+            Assert.AreEqual("Київ", articles[0].SubjectCategory);
+            Assert.AreEqual("Київ", articles[1].SubjectCategory);
+            Assert.AreEqual("Київ", articles[2].SubjectCategory);
+
+            addCategoryCommand.Undo();
+
+            Assert.AreEqual("Львів", articles[0].SubjectCategory);
+            Assert.AreEqual("Банки", articles[1].SubjectCategory);
+            Assert.AreEqual("Новини конкурентів", articles[2].SubjectCategory);
+
+
+        }
+
+        [TestMethod]
+        public void RedoTest()
+        {
+            IList<Article> articles;
+
+            var addCategoryCommand = CreateStubAddCategoryCommand(out articles);
+            addCategoryCommand.Do();
+            addCategoryCommand.Undo();
+            addCategoryCommand.Redo();
+
+            Assert.AreEqual("Київ", articles[0].SubjectCategory);
+            Assert.AreEqual("Київ", articles[1].SubjectCategory);
+            Assert.AreEqual("Київ", articles[2].SubjectCategory);
+
+        }
+    }
+}
